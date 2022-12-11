5634f7711e1a2e3756a288cdc2ac7b784e7aa835
aidangraff <graff@hartford.edu>
2022-12-11 00:30:44 -0500

    added js

---
+++ scripts/main.js
@@ -0,0 +1,22 @@
+let menuAppear = document.getElementById(`main-menu`);
+let hiddenMenu = document.querySelector(`nav`);
+let modal = document.getElementById(`modal-menu`);
+let hiddenModal = document.querySelector(`div`);
+
+menuAppear.addEventListener (`click`, () => {
+    if ((hiddenMenu.classList.contains(`hide`))) {
+        hiddenMenu.classList.remove(`hide`);
+    }
+    else {
+        hiddenMenu.classList.add(`hide`);
+    }});
+
+modal.addEventListener (`click`, () => {
+    if ((hiddenModal.classList.contains(`hidden`))) {
+        hiddenModal.classList.remove(`hidden`);
+    }
+    else {
+        hiddenModal.classList.add(`hidden`);
+    }});
+
+
